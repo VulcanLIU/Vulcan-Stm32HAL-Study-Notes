42b09999a3a8b89184038d7759679101865916ea
VulcanLiu <VulcanLiu@outlook.com>
2020-10-21 15:20:12 +0800

    dd

--- 08-EXTI/Src/main.c
+++ 08-EXTI/Src/main.c
@@ -201,7 +201,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
@@ -219,7 +219,10 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  
+}
 /* USER CODE END 4 */
 
 /**
